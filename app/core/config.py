14642e1e6f498ef3b3713ead2838d66ea7c9c60b
IvanMonichev <risen15@yandex.ru>
2025-10-13 23:09:42 +0300

    feat: update main

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,27 @@
+from pathlib import Path
+
 from pydantic.v1 import BaseSettings
 
 
 class Settings(BaseSettings):
     app_name: str = 'Glossary API'
+    app_host: str = "0.0.0.0"
     app_port: int = 8000
     app_version: str
     app_description: str = 'API для управления терминами и определениями'
     db_url: str
+    app_env: str = "dev"
+
+    @property
+    def is_dev(self) -> bool:
+        return self.app_env.lower() == "dev"
+
+    @property
+    def is_prod(self) -> bool:
+        return self.app_env.lower() == "prod"
 
     class Config:
-        env_file = ".env"
+        env_file = Path(__file__).resolve().parent.parent.parent / ".env"
         env_file_encoding = 'utf-8'
 
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import logging
-
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
+from uvicorn.server import logger
 
 from app.core.config import settings
 from app.core.database import Base, engine
@@ -9,23 +9,38 @@ from app.routers.glossary_router import router as glossary_router
 
 Base.metadata.create_all(bind=engine)
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    host = settings.app_host
+    port = settings.app_port
+    logger.info(f"Environment: {settings.app_env.upper()}")
+    logger.info(f"Swagger UI: http://{host}:{port}/docs")
+    logger.info(f"ReDoc:      http://{host}:{port}/redoc")
+    logger.info(f"OpenAPI:    http://{host}:{port}/openapi.json")
+    yield
+    logger.info("Application shutdown...")
+
+
 app = FastAPI(
     title="Glossary API",
     version="1.0.0",
     description="REST API для управления терминами",
-    contact={"name": "Ivan Monichev", "email": "ivan@monichev.ru", "url": "https://github.com/IvanMonichev"}
+    contact={
+        "name": "Ivan Monichev",
+        "email": "ivan@monichev.ru",
+        "url": "https://github.com/IvanMonichev"
+    },
+    lifespan=lifespan
 )
 
 app.include_router(glossary_router, prefix="/api", tags=["Glossary"])
 
-logger = logging.getLogger("uvicorn")
-
-
-@app.on_event("startup")
-def log_docs_urls():
-    host = "127.0.0.1"
-    port = 8000
-    logger.info(f"Swagger UI: http://{host}:{port}/docs")
-    logger.info(f"ReDoc:      http://{host}:{port}/redoc")
-    logger.info(f"OpenAPI:    http://{host}:{port}/openapi.json")
-
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host=settings.app_host,
+        port=settings.app_port,
+        reload=settings.is_dev,
+        log_level="debug" if settings.is_dev else "info",
+    )
