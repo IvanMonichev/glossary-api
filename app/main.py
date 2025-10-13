50f95b581a3f3b4f1bf2998fa72fc33f36ca4f73
IvanMonichev <risen15@yandex.ru>
2025-10-13 22:44:04 +0300

    feat: add migration and log

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+import logging
+
+import uvicorn
 from fastapi import FastAPI
+
+from app.core.config import settings
+from app.core.database import Base, engine
 from app.routers.glossary_router import router as glossary_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Glossary API",
@@ -9,3 +17,15 @@ app = FastAPI(
 )
 
 app.include_router(glossary_router, prefix="/api", tags=["Glossary"])
+
+logger = logging.getLogger("uvicorn")
+
+
+@app.on_event("startup")
+def log_docs_urls():
+    host = "127.0.0.1"
+    port = 8000
+    logger.info(f"Swagger UI: http://{host}:{port}/docs")
+    logger.info(f"ReDoc:      http://{host}:{port}/redoc")
+    logger.info(f"OpenAPI:    http://{host}:{port}/openapi.json")
+
